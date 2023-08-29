026fdbf5a230e793832c4f9b426f137054a9736c
Andrius <andrius.rochalazarino@gmail.com>
2023-08-29 10:14:17 -0400

    feat: input

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Text, TextInput, View } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Oque você quer assistir hoje</Text>
+      <View style={styles.containerInput}>
+        <TextInput placeholder="Buscar" />
+      </View>
+    </View>
+  );
+}
