25af4664a308e456d46c16d1323c6063c257fffc
Andrius <andrius.rochalazarino@gmail.com>
2023-08-31 14:12:12 -0400

    feat: search

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,10 @@
-import { ActivityIndicator, FlatList, Text, TextInput, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { styles } from "./styles";
 import { MagnifyingGlass } from "phosphor-react-native";
 import { useEffect, useState } from "react";
@@ -14,8 +20,11 @@ interface Movie {
 
 export function Home() {
   const [discoveryMovies, setDiscoveryMovies] = useState<Movie[]>([]);
+  const [searchResultMovies, setSearchResultMovies] = useState<Movie[]>([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [noResult, setNoResult] = useState(false);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     loadMoreData();
@@ -33,6 +42,33 @@ export function Home() {
     setLoading(false);
   };
 
+  const searchMovies = async (query) => {
+    setLoading(true);
+
+    const response = await api.get("/search/movie", {
+      params: {
+        query,
+      },
+    });
+
+    if (response.data.results.lenght === 0) {
+      setLoading(false);
+      return setNoResult(true);
+    }
+    setLoading(false);
+    return setSearchResultMovies(response.data.results);
+  };
+
+  const handleSearch = (text: string) => {
+    setSearch(text);
+    if (text.length > 2) {
+      return searchMovies(text);
+    }
+    return setSearchResultMovies([]);
+  };
+
+  const movieData = search.length > 2 ? searchResultMovies : discoveryMovies;
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -42,13 +78,15 @@ export function Home() {
             placeholderTextColor="#FFF"
             placeholder="Buscar"
             style={styles.input}
+            value={search}
+            onChangeText={handleSearch}
           />
           <MagnifyingGlass color="#FFF" size={25} weight="light" />
         </View>
       </View>
       <View style={styles.containerList}>
         <FlatList
-          data={discoveryMovies}
+          data={movieData}
           numColumns={3}
           renderItem={(item) => <CardMovies data={item.item} />}
           showsVerticalScrollIndicator={false}
