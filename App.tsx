af7583fecf01a8e4164bbab406b90a66bc01391c
Andrius <andrius.rochalazarino@gmail.com>
2023-09-12 09:17:51 -0400

    feat: context

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Home } from './src/screens/Home';
-import { Routes } from './src/routes';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Home } from "./src/screens/Home";
+import { Routes } from "./src/routes";
+import { MovieProvider } from "./src/contexts/MoviesContext";
 
 export default function App() {
   return (
     <>
-      <Routes />
-      <StatusBar style="auto" />
+      <MovieProvider>
+        <Routes />
+        <StatusBar style="auto" translucent backgroundColor="#242a32" />
+      </MovieProvider>
     </>
   );
 }
